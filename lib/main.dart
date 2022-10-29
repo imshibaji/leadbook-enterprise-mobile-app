33a9b90ba6e6a6a640298961c87a720a21857bed
Shibaji Debnath <imshibaji@gmail.com>
2022-10-29 23:07:32 +0530

    Modified

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:leadbook_online/app/themes/app_theme.dart';
 
 import 'app/data/auth.dart';
 import 'app/routes/app_pages.dart';
+import 'app/themes/app_theme.dart';
 import 'generated/locales.g.dart';
 
-void main() async {
+Future<void> initialized() async {
   await GetStorage.init();
   await Get.putAsync((() => Auth().init()));
-
   await dotenv.load(fileName: ".env");
+}
+
+void main() async {
+  await initialized();
+
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
